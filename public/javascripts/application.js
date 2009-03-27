584f329a4ee89777e6c42b22867255f5a9d7fa22
deepthawtz <dylan@deepthawtz.local>
2009-03-27 14:08:54 -0700

    warn deletion is permanent.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,7 @@
 // confirm deletion
 $(document).ready(function () {
   $('.delete').click(function() {
-    var answer = confirm('Are you sure?');
+    var answer = confirm('Deletion will be permanent. Are you sure you want this?');
     return answer;
   });
 });
