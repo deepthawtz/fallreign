2387061fde474bca40fc094ec753034add86d6d9
deepthawtz <dylan@deepthawtz.local>
2009-04-06 15:58:41 -0700

    lightbox added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,5 +5,9 @@ $(document).ready(function () {
   $('.delete').click(function() {
     var answer = confirm('Deletion will be permanent. Are you sure you want this?');
     return answer;
-  });
+  });               
+  $("#gallery").lightBox({
+    fixedNavigation:true,
+    imageLoading: 'images/loadingAnimation.gif'    
+  })
 });
