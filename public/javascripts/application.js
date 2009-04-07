d6457f3e4bca52a25496574027a9efc172c55c80
deepthawtz <dylan@deepthawtz.local>
2009-04-06 17:20:08 -0700

    listing artists sideways. test

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,7 +5,13 @@ $(document).ready(function () {
   $('.delete').click(function() {
     var answer = confirm('Deletion will be permanent. Are you sure you want this?');
     return answer;
-  });                     
+  }); 
+  
+  $('#menu').lavaLamp({
+    fx: 'easeOutBounce',
+    speed: 700,
+    autoReturn: false
+  });                    
   
   $("#gallery a").lightBox({
     imageLoading: 'images/loadingAnimation.gif'    
