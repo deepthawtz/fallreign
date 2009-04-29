7af513e4c384fc6b8d5627938fc20f3229cbbc3c
deepthawtz <dylan@deepthawtz.local>
2009-04-28 18:50:00 -0700

    adding mimetype for .xspf

--- config/init.rb
+++ config/init.rb
@@ -8,17 +8,15 @@ use_template_engine :haml
  
 Merb::Config.use do |c|
   c[:use_mutex] = false
-  c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
-  
-  # cookie session store configuration
+  c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper    
   c[:session_secret_key]  = '9d02fc5bba73a396aee1ff7e9ab9ea6c44d87073'  # required for cookie session store
   c[:session_id_key] = '_fallreign_session_id' # cookie session id key, defaults to "_session_id"
 end
  
-Merb::BootLoader.before_app_loads do
-  # This will get executed after dependencies have been loaded but before your app's classes have loaded.
+Merb::BootLoader.before_app_loads do  
 end
  
-Merb::BootLoader.after_app_loads do
-  # This will get executed after your app's classes have been loaded.
-end
+Merb::BootLoader.after_app_loads do  
+end                                 
+
+Merb.add_mime_type :xspf, :to_xml, %w[application/xspf+xml]
